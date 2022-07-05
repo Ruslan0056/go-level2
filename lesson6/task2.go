55cba476850b40fa82d68fc7f055db35e24d9f02
Руслан <rus94.5610@gmail.com>
2022-07-05 19:53:52 +0300

    dz6

---
+++ lesson6/task2.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+const count = 10
+
+func main() {
+	var (
+		lock sync.Mutex
+		ch   = make(chan int)
+		done = make(chan struct{})
+		wg = sync.WaitGroup
+	)
+
+	wg.Add(count)
+		for i := 0; i <= count; i += 1 {
+		go func() {
+			defer close(done)
+			defer wg.Done()
+			lock.Lock()
+			defer lock.Unlock()
+			ch <- i
+		}()
+		sec := <-ch
+		fmt.Println(sec)
+	}
+		wg.Add(1)
+		go func(i int) {
+		defer wg.Done()
+		for {
+		select {
+		case v := <-ch:
+		fmt.Println(v)
+		case <-done:
+		return
+		}
+		}
+		}(i)
+		}
+		wg.Wait()
+
+}
